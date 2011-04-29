f22f450daeb98d537e447f6e0d744723a8dd72c5
Chris Amico <eyeseast@gmail.com>
2011-04-28 23:33:43 -0400

    Added a SHAPEFILES_DIR setting

--- dcgis/settings.py
+++ dcgis/settings.py
@@ -128,6 +128,8 @@ INSTALLED_APPS = (
     'boundaryservice',
 )
 
+SHAPEFILES_DIR = f('data/shapefiles')
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
